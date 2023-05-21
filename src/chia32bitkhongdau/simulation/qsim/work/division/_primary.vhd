72186911f8f684dc3f31e4d1c865bb1ae1984b68
Bao <21521860@mg.uit.edu.vn>
2023-05-21 20:13:27 +0700

    final

--- src/chia32bitkhongdau/simulation/qsim/work/division/_primary.vhd
+++ src/chia32bitkhongdau/simulation/qsim/work/division/_primary.vhd
@@ -3,10 +3,10 @@ use verilog.vl_types.all;
 entity division is
     port(
         Done            : out    vl_logic;
-        S               : in     vl_logic;
+        Start           : in     vl_logic;
         CLK             : in     vl_logic;
-        B               : in     vl_logic_vector(31 downto 0);
         A               : in     vl_logic_vector(31 downto 0);
+        B               : in     vl_logic_vector(31 downto 0);
         Qu              : out    vl_logic_vector(63 downto 0);
         R               : out    vl_logic_vector(63 downto 0)
     );
--- src/chia32bitkhongdau/simulation/qsim/work/division_vlg_sample_tst/_primary.vhd
+++ src/chia32bitkhongdau/simulation/qsim/work/division_vlg_sample_tst/_primary.vhd
@@ -5,7 +5,7 @@ entity division_vlg_sample_tst is
         A               : in     vl_logic_vector(31 downto 0);
         B               : in     vl_logic_vector(31 downto 0);
         CLK             : in     vl_logic;
-        S               : in     vl_logic;
+        Start           : in     vl_logic;
         sampler_tx      : out    vl_logic
     );
 end division_vlg_sample_tst;
