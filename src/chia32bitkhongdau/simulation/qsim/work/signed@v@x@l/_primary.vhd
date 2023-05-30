fb02c4221d7fa39233502894590647b284b16478
Bao <21521860@mg.uit.edu.vn>
2023-05-30 09:43:20 +0700

    30/5

---
+++ src/chia32bitkhongdau/simulation/qsim/work/signed@v@x@l/_primary.vhd
@@ -0,0 +1,13 @@
+library verilog;
+use verilog.vl_types.all;
+entity signedVXL is
+    port(
+        Done            : out    vl_logic;
+        Start           : in     vl_logic;
+        CLK             : in     vl_logic;
+        A               : in     vl_logic_vector(31 downto 0);
+        B               : in     vl_logic_vector(31 downto 0);
+        Qu              : out    vl_logic_vector(63 downto 0);
+        R               : out    vl_logic_vector(63 downto 0)
+    );
+end signedVXL;
---
+++ src/chia32bitkhongdau/simulation/qsim/work/signed@v@x@l_vlg_vec_tst/_primary.vhd
@@ -0,0 +1,4 @@
+library verilog;
+use verilog.vl_types.all;
+entity signedVXL_vlg_vec_tst is
+end signedVXL_vlg_vec_tst;
